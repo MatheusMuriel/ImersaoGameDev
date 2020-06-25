9c60caf3a0931c144725cfaeee96f67cc8039681
Matheus Muriel <matheus.muriel@outlook.com>
2020-06-25 13:59:27 -0300

    Personagem

--- sketch.js
+++ sketch.js
@@ -1,8 +1,28 @@
+let imagemCenario;
+let imagemPersonagem;
+
+function preload() {
+  imagemCenario = loadImage('imagens/cenario/floresta.png');
+  imagemPersonagem = loadImage('imagens/personagem/correndo.png');
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(200);
-  circle(mouseX, mouseY, 200);
+  image(imagemCenario, 0, 0, width, height);
+
+  /**
+   * Parametros: 
+   * cordX
+   * cordY
+   * altura
+   * largura
+   * cordDentroDaImagemX
+   * cordDentroDaImagemY
+   * alturaDentroDaImagem
+   * larguraDentroDaImagem
+   */
+  image(imagemPersonagem, 0, height - 135, 110, 135, 0, 0, 220, 270);  
 }
