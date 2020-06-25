7151708f81bb502e8ee9e6ad56cd192724d7a433
Matheus Muriel <matheus.muriel@outlook.com>
2020-06-25 14:12:08 -0300

    Rolagem do cenario

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 let imagemCenario;
 let imagemPersonagem;
+let cenario;
 
 function preload() {
   imagemCenario = loadImage('imagens/cenario/floresta.png');
@@ -8,21 +9,38 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  cenario = new Cenario(imagemCenario, 3);
 }
 
 function draw() {
-  image(imagemCenario, 0, 0, width, height);
-
-  /**
-   * Parametros: 
-   * cordX
-   * cordY
-   * altura
-   * largura
-   * cordDentroDaImagemX
-   * cordDentroDaImagemY
-   * alturaDentroDaImagem
-   * larguraDentroDaImagem
-   */
+  cenario.exibe();
+  cenario.move();
   image(imagemPersonagem, 0, height - 135, 110, 135, 0, 0, 220, 270);  
+}
+
+class Cenario {
+  constructor(image, velocidade){
+    this.imagem = image;
+    this.velocidade = velocidade;
+    this.x1 = 0;
+    this.x2 = width;
+  }
+
+  exibe() {
+    image(this.imagem, this.x1, 0, width, height);
+    image(this.imagem, this.x2, 0, width, height);
+  }
+
+  move() {
+    this.x1 = this.x1 - this.velocidade;
+    this.x2 = this.x2 - this.velocidade;
+
+    if (this.x1 < -width) {
+      this.x1 = width;
+    }
+
+    if (this.x2 < -width) {
+      this.x2 = width;
+    }
+  }
 }
