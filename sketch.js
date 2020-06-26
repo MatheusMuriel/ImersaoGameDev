896543faaaa2cf6890e656c72ad9da0c7e53496c
Matheus Muriel <matheus.muriel@outlook.com>
2020-06-26 19:37:08 -0300

    Versão final

--- sketch.js
+++ sketch.js
@@ -1,168 +1,29 @@
-let imagemCenario;
-let imagemGameOver;
-let imagemPersonagem;
-let imagemInimigo;
-let imagemInimigoGrande;
-let imagemInimigoVoador;
-
-let somDoJogo;
-let somDoPulo;
-let cenario;
-let personagem;
-let inimigo;
-let inimigoGrande;
-let inimigoVoador;
-
-const matrizInimigo = [
-  [0, 0],
-  [104, 0],
-  [208, 0],
-  [312, 0],
-  [0, 104],
-  [104, 104],
-  [208, 104],
-  [312, 104],
-  [0, 208],
-  [104, 208],
-  [208, 208],
-  [312, 208],
-  [0, 312],
-  [104, 312],
-  [208, 312],
-  [312, 312],
-  [0, 416],
-  [104, 416],
-  [208, 416],
-  [312, 416],
-  [0, 520],
-  [104, 520],
-  [208, 520],
-  [312, 520],
-  [0, 624],
-  [104, 624],
-  [208, 624],
-  [312, 624],
-];
-
-const matrizPersonagem = [
-  [0, 0],
-  [220, 0],
-  [440, 0],
-  [660, 0],
-  [0, 270],
-  [220, 270],
-  [440, 270],
-  [660, 270],
-  [0, 540],
-  [220, 540],
-  [440, 540],
-  [660, 540],
-  [0, 810],
-  [220, 810],
-  [440, 810],
-  [660, 810],
-];
-
-const matrizInimigoGrande = [
-  [0,0],
-  [400,0],
-  [800,0],
-  [1200,0],
-  [1600,0],
-  [0,400],
-  [400,400],
-  [800,400],
-  [1200, 400],
-  [1600, 400],
-  [0,800],
-  [400, 800],
-  [800, 800],
-  [1200, 800],
-  [1600, 800],
-  [0, 1200],
-  [400, 1200],
-  [800, 1200],
-  [1200, 1200],
-  [1600, 1200], 
-  [0, 1600],
-  [400, 1600],
-  [800, 1600],
-  [1200, 1600],
-  [1600, 1600],
-  [0, 2000],
-  [400, 2000],
-  [800, 2000],
-];
-
-const matrizInimigoVoador = [
-  [0,0],
-  [200, 0],
-  [400, 0],
-  [0, 150],
-  [200, 150],
-  [400, 150],
-  [0, 300],
-  [200, 300],
-  [400, 300],
-  [0, 450],
-  [200, 450],
-  [400, 450],
-  [0, 600],
-  [200, 600],
-  [400, 600],
-  [0, 750],
-];
-
-function preload() {
-  somDoJogo = loadSound('sons/trilha_jogo.mp3');
-  somDoPulo = loadSound('sons/somPulo.mp3');
-
-  imagemCenario = loadImage('imagens/cenario/floresta.png');
-  imagemGameOver = loadImage('imagens/assets/game-over.png');
-
-  imagemPersonagem = loadImage('imagens/personagem/correndo.png');
-  imagemInimigo = loadImage('imagens/inimigos/gotinha.png');
-  imagemInimigoGrande = loadImage('imagens/inimigos/troll.png');
-  imagemInimigoVoador = loadImage('imagens/inimigos/gotinha-voadora.png');
-}
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  cenario = new Cenario(imagemCenario, 3);
-  personagem = new Personagem(matrizPersonagem, imagemPersonagem, 0, 30, 110, 135, 220, 270);
 
-  inimigo = new Inimigo(matrizInimigo, imagemInimigo, 
-    width - 52, 30, 52, 52, 104, 104, 8, 100);
-  inimigoVoador = new Inimigo(matrizInimigoVoador, imagemInimigoVoador, 
-    width - 52, 200, 100, 75, 200, 150, 8, 130);
-  inimigoGrande = new Inimigo(matrizInimigoGrande, imagemInimigoGrande,
-     width, 0, 200, 200, 400, 400, 5, 500);
-  
+  jogo = new Jogo();
+  jogo.setup();
+
+  telaInicial = new TelaInicial();
+
   frameRate(40);
+  cenas = {
+    jogo,
+    telaInicial,
+  };
+
+  botaoGerenciador = new BotaoGerenciador("Start Game", width / 2, height / 2);
+  
+  
+
   //somDoJogo.loop();
+  somDoJogo.setVolume(0.1);
 }
 
 function keyPressed() {
-  if (key === 'ArrowUp') {
-    personagem.pula();
-    somDoPulo.play();
-  }
+  jogo.keyPress(key);
 }
 
 function draw() {
-  cenario.exibe();
-  cenario.move();
-  personagem.exibe();
-  personagem.aplicaGravidade();
-  inimigo.exibe();
-  inimigo.move();
-  inimigoVoador.exibe();
-  inimigoVoador.move();
-  inimigoGrande.exibe();
-  inimigoGrande.move();
-
-  if (personagem.estaColidindo(inimigo)) {
-    //image(imagemGameOver, (width/2 - (412/2)), (height/2 - (78/2)), 412, 78);
-    //noLoop();
-  }
+  cenas[cenaAtual].draw();
 }
